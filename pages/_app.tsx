05538b0af46cc94f14b190c70e0528af54f3b729
elpiarthera <artherasmg@gmail.com>
2024-10-04 17:51:45 +0200

    Update CSS structure, fix Tailwind configuration, and clean up project files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,20 @@
 import '../styles/globals.css'
 import '../styles/components.css'
 import type { AppProps } from 'next/app'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { AppProvider } from '@/lib/context/AppContext'
+import { ThemeProvider } from '@/lib/hooks/useTheme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <AppProvider>
+        <Component {...pageProps} />
+        <ToastContainer />
+      </AppProvider>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
